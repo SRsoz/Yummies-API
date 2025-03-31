5af059c5c1c353b21de30794a0dfa09b0746886e
SRsoz <sozyar.rauf@chasacademy.se>
2025-03-31 17:10:36 +0200

    admin

--- src/controllers/usercontroller.ts
+++ src/controllers/usercontroller.ts
@@ -54,7 +54,7 @@ export const loginUser = async (req: Request, res: Response): Promise<void> => {
             return;
         }
 
-        const token = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: '1h' });
+        const token = jwt.sign({ id: user._id, role: user.role}, JWT_SECRET, { expiresIn: '1h' });
         res.json({ token });
         return
     } catch (error) {
